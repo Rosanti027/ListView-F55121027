eeb7c7c091f7cb88cde84210e8179bb4ac157c29
Rosanti <rossanti611@gmail.com>
2022-11-20 19:08:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mylistview_f55121027_rosanti/dETAIL.java
@@ -0,0 +1,14 @@
+package com.example.mylistview_f55121027_rosanti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dETAIL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
